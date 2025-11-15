d7e5e4cf3f908ac15b6386a45bdbf7da27d78fc3
al8273j <139778039+al8273j@users.noreply.github.com>
2025-11-15 13:31:13 -0500

    first commit

---
+++ src/main/java/project/Main.java
@@ -0,0 +1,37 @@
+package project;
+
+import project.data.getData;
+
+public class Main {
+    public static void main(String[] args) {
+        //validates number of runtime arguments
+        if (args.length != 4) {
+            //throw error
+        }
+        String parkingViolationsFormat = args[0];
+        //validates file format input
+        if (!(parkingViolationsFormat.equals("csv") || parkingViolationsFormat.equals("json"))) {
+            //throw error
+        }
+        String parkingViolationsFile = args[1];
+        String propertyValuesFile = args[2];
+        String populationFile = args[3];
+
+
+        //Data Tier
+        getData data = new getData(parkingViolationsFormat, parkingViolationsFile, propertyValuesFile, populationFile);
+
+        /*
+        * Processor Tier
+        * processData processor = new processData(data);
+        *
+        * UI Tier
+        * userInterface ui = new userInterface(processor);
+        * */
+
+
+
+
+    }
+}
+
---
+++ src/main/java/project/data/getData.java
@@ -0,0 +1,40 @@
+package project.data;
+
+import org.json.simple.*;
+
+public class getData {
+
+    String parkingViolationsFormat;
+    String parkingViolationsFile;
+    String propertyValuesFile;
+    String populationFile;
+
+
+    public getData(String parkingViolationsFormat, String parkingViolationsFile, String propertyValuesFile, String populationFile) {
+        this.parkingViolationsFormat = parkingViolationsFormat;
+        this.parkingViolationsFile = parkingViolationsFile;
+        this.propertyValuesFile = propertyValuesFile;
+        this.populationFile = populationFile;
+    }
+
+    public String getParkingViolationsFormat() {
+        return parkingViolationsFormat;
+    }
+
+    public JSONArray getParkingViolationsJSON() {
+        //
+    }
+
+    public JSONArray getPropertyValuesCSV() {
+        //
+    }
+
+
+
+
+
+
+
+
+
+}

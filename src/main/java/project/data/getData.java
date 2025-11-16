fc2067c80e7346466406aa72bee313c63ed8e356
al8273j <139778039+al8273j@users.noreply.github.com>
2025-11-16 12:35:21 -0500

    created JSON Array method for retrieving parkingViolations and made method for processing populationArray txt

--- src/main/java/project/data/getData.java
+++ src/main/java/project/data/getData.java
@@ -1,6 +1,12 @@
 package project.data;
 
 import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class getData {
 
@@ -21,9 +27,21 @@ public class getData {
         return parkingViolationsFormat;
     }
 
-    /*public JSONArray getParkingViolationsJSON() {
+    public JSONArray getParkingViolationsJSON() throws IOException, ParseException {
+        JSONArray parkingViolationsJSON = (JSONArray) new JSONParser().parse(new FileReader(parkingViolationsFile));
+        return parkingViolationsJSON;
+    }
 
-    }*/
+    public ArrayList<String[]> getPopulationArray() throws IOException {
+        ArrayList<String[]> populationArray = new ArrayList<>();
+        FileReader fileReader = new FileReader(populationFile);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line;
+        while((line = bufferedReader.readLine())!= null){
+            populationArray.add(line.split(" "));
+        }
+        return populationArray;
+    }
 
 
 

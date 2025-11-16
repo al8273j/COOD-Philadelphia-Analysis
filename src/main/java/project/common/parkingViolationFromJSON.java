4d84290b4cee8b80f9ab3f21b0667e46d9b6a4c2
al8273j <139778039+al8273j@users.noreply.github.com>
2025-11-16 13:02:02 -0500

    updated return types of get methods

---
+++ src/main/java/project/common/parkingViolationFromJSON.java
@@ -0,0 +1,40 @@
+package project.common;
+
+import org.json.simple.JSONObject;
+
+public class parkingViolationFromJSON {
+
+
+    private JSONObject parkingViolation;
+
+    public parkingViolationFromJSON(JSONObject parkingViolation) {
+        this.parkingViolation = parkingViolation;
+    }
+
+    public long getTicketNumber(){
+        return (long)parkingViolation.get("ticket_number");
+    }
+
+    public String getPlateId(){
+        return (String)parkingViolation.get("plate_id");
+    }
+    public String getDate(){
+        return (String)parkingViolation.get("date");
+    }
+
+    public String getZipCode(){
+        return (String)parkingViolation.get("zip_code");
+    }
+
+    public String getViolation(){
+        return (String)parkingViolation.get("violation");
+    }
+
+    public long getFine(){
+        return (long)parkingViolation.get("fine");
+    }
+
+    public String getState(){
+        return (String)parkingViolation.get("state");
+    }
+}
--- src/main/java/project/common/populationDataEntry.java
+++ src/main/java/project/common/populationDataEntry.java
@@ -1,15 +1,15 @@
 package project.common;
 
-public class getPopulationData {
+public class populationDataEntry {
 
     private String[] populationData;
-    public getPopulationData(String[] populationData) {
+    public populationDataEntry(String[] populationData) {
         this.populationData = populationData;
     }
 
 
-    public int getZipCode(){
-        return Integer.parseInt(populationData[0]);
+    public String getZipCode(){
+        return populationData[0];
     }
 
     public int getPopulationNumber(){

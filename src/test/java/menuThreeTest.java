6887db53e28c24c21e205d31eb04d080848c6149
al8273j <139778039+al8273j@users.noreply.github.com>
2025-12-10 13:42:09 -0500

    added more menuThree Testcases for 100% line coverage

--- src/test/java/menuThreeTest.java
+++ src/test/java/menuThreeTest.java
@@ -9,6 +9,7 @@ import project.processor.processData;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.TreeMap;
 import java.util.concurrent.ExecutionException;
@@ -33,6 +34,48 @@ public class menuThreeTest {
 
 
         assertEquals(500000,p.menuThree("11216"));
+    }
+
+
+    @Test
+    public void menuThreeTestNoMemo() throws IOException, ParseException, ExecutionException, InterruptedException {
+        processData p = new processData(dataCSV);
+        p.properties = new ArrayList<>();
+
+        String[] arr1 = new String[73];
+        Arrays.fill(arr1, "");
+        arr1[34] = "2000";
+        arr1[72] = "11216";
+        p.properties.add(new propertiesDataEntry(arr1));
+
+        String[] arr2 = new String[73];
+        Arrays.fill(arr2, "");
+        arr2[34] = "1000";
+        arr2[72] = "11216";
+        p.properties.add(new propertiesDataEntry(arr2));
+
+        assertEquals(1500,p.menuThree("11216"));
+    }
+
+
+    @Test
+    public void menuThreeTestNoMatchingZip() throws IOException, ParseException, ExecutionException, InterruptedException {
+        processData p = new processData(dataCSV);
+        p.properties = new ArrayList<>();
+
+        String[] arr1 = new String[73];
+        Arrays.fill(arr1, "");
+        arr1[34] = "2000";
+        arr1[72] = "11216";
+        p.properties.add(new propertiesDataEntry(arr1));
+
+        String[] arr2 = new String[73];
+        Arrays.fill(arr2, "");
+        arr2[34] = "1000";
+        arr2[72] = "11216";
+        p.properties.add(new propertiesDataEntry(arr2));
+
+        assertEquals(0,p.menuThree("11217"));
     }
 
 
